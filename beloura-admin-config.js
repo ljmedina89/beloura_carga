296752de29e07536bf2c6697af92710732a4abcb
ljmedina89 <leomedinaricachi@gmail.com>
2026-08-16 13:58:36 -0400

    Update API URL in beloura-admin-config.js

--- beloura-admin-config.js
+++ beloura-admin-config.js
@@ -1,4 +1,4 @@
 window.BELOURA_ADMIN_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbxqqb8J_lskdOci83bRVc62eLjQODphDbfa-8NxKY2ixGlJMg-P7jJS36Ly9XkhA-ONFg/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzhy98J5pvGjJIPeNTWB5gz28E9muG9iS0pGR6NKuU7URtkMMj0iE_UZUXXCGIoaL5vRQ/exec",
   adminSecret: "Beloura2025@"
 };

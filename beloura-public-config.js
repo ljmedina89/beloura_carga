feace2554f87dbee08b479e652b38f1b7d23016b
ljmedina89 <leomedinaricachi@gmail.com>
2026-08-17 19:35:39 -0400

    Fix API URL in beloura-public-config.js

--- beloura-public-config.js
+++ beloura-public-config.js
@@ -1,4 +1,4 @@
 window.BELOURA_PUBLIC_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzHECZoeutKHEau4d4AUFeeXu5VaaXXsAZzCk8YPA-_9EZ64Io8lhtOB74O5sNrspDdng/execc",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzHECZoeutKHEau4d4AUFeeXu5VaaXXsAZzCk8YPA-_9EZ64Io8lhtOB74O5sNrspDdng/exec",
   trackingBaseUrl: "https://trk.belourastore.com/?codigo="
 };

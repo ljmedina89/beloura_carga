e51b3a3fc104dbe3e117acc0692e5e18302e4a69
ljmedina89 <leomedinaricachi@gmail.com>
2026-08-18 19:38:23 -0400

    Update beloura-public-config.js

--- beloura-public-config.js
+++ beloura-public-config.js
@@ -1,4 +1,4 @@
 window.BELOURA_PUBLIC_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwnW0dOWaYKqCTVJWA7qKNq91cQ_mirCfx0rkP0FjbKuzjhPxki0mM-83960WwwOpOScg/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzl3aFcS2S8tHtGO9OHJ8SEYKk422dNYJ34fSaCf_goFSa0HfvJTAfydQ8435yHH8ZDvg/exec",
   trackingBaseUrl: "https://trk.belourastore.com/?codigo="
 };
